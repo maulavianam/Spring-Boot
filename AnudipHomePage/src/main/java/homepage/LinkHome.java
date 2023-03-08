0016ef6e573513a2907533e9d51b289864757f88
maulavianam <121223600+maulavianam@users.noreply.github.com>
2023-03-09 02:15:44 +0530

    Add files via upload

---
+++ AnudipHomePage/src/main/java/homepage/LinkHome.java
@@ -0,0 +1,21 @@
+package homepage;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//RestController annotation is used to create RESTful web services using Spring MVC
+@RestController
+public class LinkHome 
+{
+	//http://localhost:1001/link
+	
+	//RequestMapping annotation is used to map web requests onto specific handler classes and/or handler methods. 
+	@RequestMapping("/Hplink")
+	public String Hplink()
+	{
+		return "<body bgcolor='Orange', text='Black'><center><h1> Anudip Foundation </h1></center>\"\r\n"
+				+"<center><h2> Home Page Link </h2></center>\"\r\n"
+				+"<center><h3><a href=https:aln.anudip.org style='color:green'>https:aln.anudip.org</a></h3></center></body>";
+	}
+
+}
